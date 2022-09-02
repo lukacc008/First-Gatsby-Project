ac768b022b567c9e9a011972efb677a2540c9d81
unknown <lukacc008@gmail.com>
2022-09-02 16:28:42 +0200

    Created Layout file to wrap some components with {children} prop

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react"
+import Layout from "./components/Layout"
 
 export default function About() {
   return (
-    <div>
+    <Layout>
+      <div>
         <h1>About Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sequi voluptas error ad? Quasi, maxime? Error distinctio eligendi eos quaerat non hic aliquid quo commodi, doloribus id ullam aperiam rem.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur inventore dolor possimus ad, ipsam incidunt. Tempora ea eveniet aut vitae architecto eum perspiciatis? Labore enim distinctio cum sequi error dolore.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum recusandae vel reprehenderit ex debitis amet exercitationem numquam at, architecto inventore, doloribus nobis est distinctio quisquam dolores! Nam nobis officia maiores!</p>
-    </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sequi
+          voluptas error ad? Quasi, maxime? Error distinctio eligendi eos
+          quaerat non hic aliquid quo commodi, doloribus id ullam aperiam rem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          inventore dolor possimus ad, ipsam incidunt. Tempora ea eveniet aut
+          vitae architecto eum perspiciatis? Labore enim distinctio cum sequi
+          error dolore.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
+          recusandae vel reprehenderit ex debitis amet exercitationem numquam
+          at, architecto inventore, doloribus nobis est distinctio quisquam
+          dolores! Nam nobis officia maiores!
+        </p>
+      </div>
+    </Layout>
   )
 }
