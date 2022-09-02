3d5a6ebea914740a2872d04d7fdc4c64e246779d
unknown <lukacc008@gmail.com>
2022-09-02 18:05:44 +0200

    Styling, global css. And trying to make css modules work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import Layout from "./components/Layout"
+import classes from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-    <section>
-      <div>
-        <h2>Design</h2>
-        <h3>Develop & Deploy</h3>
-        <p>UX designer & web developer based in Manchester.</p>
-      </div>
-    </section>
+      <section className={classes.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Manchester.</p>
+        </div>
+      </section>
     </Layout>
   )
 }
