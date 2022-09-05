6a001534ec61786700bbebc92a55ce94412ba161
unknown <lukacc008@gmail.com>
2022-09-05 16:53:23 +0200

    some project styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "./components/Layout"
-import classes from "../styles/home.module.css"
+import "../styles/home.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section className={classes.header}>
+      <section className="header">
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
+          <Link className="btn" to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
